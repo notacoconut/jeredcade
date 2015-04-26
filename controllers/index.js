3ca5c7be8450a1bbb44c202c594fbe0903b69d56
Jered <notacoconut@users.noreply.github.com>
2015-04-26 18:22:11 -0500

    application error - not set to heroku ports

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,15 @@
  * do something with the user model
  * var User = require('../models/user');
  */
+var app = express();
 
 exports.index = function (req, res) {
   res.render('index', {
     title: 'jeredcade'
   });
+  app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+  
 };
