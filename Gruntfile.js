6362a3235742c9bb3cd476981ba8a0ffc0044a21
Jered <notacoconut@users.noreply.github.com>
2015-04-26 21:37:21 -0500

    finally got grunt working

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
         // add your css files over here to concatenate all css files
         // let's save our site users some bandwith
         src: ['public/vendor/bootstrap/dist/css/bootstrap.min.css', 'public/css/styles.min.css'],
-+       dest: 'public/css/app.styles.min.css'
+      dest: 'public/css/app.styles.min.css'
       }
     },
     uglify: {
@@ -25,10 +25,9 @@ module.exports = function (grunt) {
       },
       target: {
         files: {
-+          // add your js files over here to minify them into one javascript source file
-+          'public/js/app.min.js': ['public/vendor/jquery/dist/jquery.min.js', 'public/vendor/bootstrap/dist/js/bootstrap.min.js', 'public/js/main.js']
-+        }
-        }
+       // add your js files over here to minify them into one javascript source file
+         'public/js/app.min.js': ['public/vendor/jquery/dist/jquery.min.js', 'public/vendor/bootstrap/dist/js/bootstrap.min.js', 'public/js/main.js']
+       }
       }
     },
     less: {
