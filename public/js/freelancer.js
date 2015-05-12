385f403505cb8fc0ee6dd10c07b33c1d0890fc10
Jered <notacoconut@users.noreply.github.com>
2015-05-11 19:55:05 -0500

    back to top button work

--- public/js/freelancer.js
+++ public/js/freelancer.js
@@ -1,9 +1,3 @@
-/*!
- * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('body').on('click', '.page-scroll a', function(event) {
@@ -14,7 +8,6 @@ $(function() {
         event.preventDefault();
     });
 });
-
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
