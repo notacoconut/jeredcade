85243f7dcdbfe78db290e2328a30163897b3efdf
Jered <notacoconut@users.noreply.github.com>
2015-05-13 21:47:26 -0500

    Revert "back to top button work"
    This reverts commit 385f403505cb8fc0ee6dd10c07b33c1d0890fc10.

--- public/js/freelancer.js
+++ public/js/freelancer.js
@@ -1,3 +1,9 @@
+/*!
+ * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('body').on('click', '.page-scroll a', function(event) {
@@ -8,6 +14,7 @@ $(function() {
         event.preventDefault();
     });
 });
+
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
