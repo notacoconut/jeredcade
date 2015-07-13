a300584ff3e8854d5e9315a9ff96e0b8dd6b5028
Jered Spears <jered.jcmaster@gmail.com>
2015-07-12 21:21:39 -0500

    hacking with psql

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,8 @@
  * var User = require('../models/user');
  */
 
+ var pg = require('pg');
+
 exports.index = function (req, res) {
   res.render('index', {
     title: 'jeredcade'
@@ -18,14 +20,27 @@ exports.subscribe = function (req, res) {
 		res.status(400).send('Bad Request');
 
 	}
-	var pg = require('pg');
-	console.log(process.env.DATABASE_URL);
+	
+
+	console.log("connecting to db @: " + process.env.DATABASE_URL);
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
-		console.log(err,client);
-  	//var query = client.query('SELECT * FROM your_table');
+	
+		//todo: add error handling!
+		//console.log(err,client);
+
+		var qtext = 'insert into subscriptions (email_address) values (\'' + email + '\');';
+  	    var query = client.query(qtext, function(error, result){
+
+  	    	console.log(error, result);
+
+  	    });
+  		
   		//query.on('row', function(row) {
-    	//console.log(JSON.stringify(row));
+    	//	console.log(JSON.stringify(row));
+		//});
+
   	});
+	
 
 	res.status(200).end();
 };
